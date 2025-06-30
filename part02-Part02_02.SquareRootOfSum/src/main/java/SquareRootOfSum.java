464618de26d068629801a203c30bd8e6128a70da
An16og <24it10an21@mitsgwl.ac.in>
2025-07-01 01:29:23 +0530

    Part 2

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,11 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a= Integer.valueOf(scanner.nextLine());
+        int b= Integer.valueOf(scanner.nextLine());
+        double root=Math.sqrt(a+b);
+        System.out.println(root);
+
 
     }
 }
