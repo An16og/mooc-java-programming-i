30c69a6fb964ca7880173f74ac98500c986f6f39
An16og <24it10an21@mitsgwl.ac.in>
2025-07-03 18:36:23 +0530

    Part 03

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -4,7 +4,7 @@ public class AdvancedAstrology {
     public static void printStars(int number) {
         // part 1 of the exercise
         for(int i=number;i>0;i--){
-            System.out.print("*");
+        System.out.print("*");
         }
         System.out.println();
     }
@@ -21,7 +21,7 @@ public class AdvancedAstrology {
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
-        for(int i=1;i<=size;i++){
+        for(int i=0;i<size;i++){
             printSpaces(size-i);
             printStars(i);
         }
@@ -37,8 +37,8 @@ public class AdvancedAstrology {
             i++;
         }
         for(int a=0;a<2;a++){
-            printSpaces(height-2);
-            printStars(3);
+        printSpaces(height-2);
+        printStars(3);
 
         }
     }
---
+++ part03-Part03_03.IndexOutOfBoundsException/src/main/java/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+    for (String line : lines) {
